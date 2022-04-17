bf3eb17b26b5d22f929308481292e93ec93c61aa
Felipe Marcolin <felipe.marcolin@hotmail.com>
2022-04-17 13:11:26 -0300

    validating cep with external api

--- lib/deliveryApi/users/create.ex
+++ lib/deliveryApi/users/create.ex
@@ -1,16 +1,17 @@
 defmodule DeliveryApi.Users.Create do
   alias DeliveryApi.{Error, Repo, User}
+  alias DeliveryApi.ViaCep.Client
 
-  def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
-    |> handle_insert()
-  end
+  def call(%{"cep" => cep} = params) do
+    user_changeset = User.changeset(params)
 
-  defp handle_insert({:ok, %User{}} = result), do: result
-
-  defp handle_insert({:error, result}) do
-    {:error, Error.build(:bad_request, result)}
+    with {:ok, %User{}} <- User.build(user_changeset),
+         {:ok, _cep_info} <- Client.get_cep_info(cep),
+         {:ok, %User{}} = user <- Repo.insert(user_changeset) do
+      user
+    else
+      {:error, %Error{}} = error -> error
+      {:error, result} -> {:error, Error.build(:bad_request, result)}
+    end
   end
 end
